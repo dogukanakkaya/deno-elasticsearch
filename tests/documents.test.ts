59caa388a16504e076ecffaed937b4423b24d12d
dogukanakkaya <doguakkaya27@hotmail.com>
2022-02-14 22:27:45 +0300

    refactor: document and indice apis tests

--- tests/documents.test.ts
+++ tests/documents.test.ts
@@ -1,76 +1,61 @@
-import { client, ELASTIC_TEST_INDEX, assertEquals } from './deps.ts'
-
-const index = ELASTIC_TEST_INDEX
+import { client, ELASTIC_TEST_INDEX, assertEquals, assert } from './deps.ts'
 
 Deno.test("document apis", async (t) => {
-    await t.step({
-        name: 'create indice for document',
-        fn: async () => {
-            const indice = await client.indices.create({ index })
-
-            assertEquals(indice.acknowledged, true)
-            assertEquals(indice.index, index)
-        }
-    })
+    // [test_dependency]
+    await client.indices.create({ index: ELASTIC_TEST_INDEX })
 
     await t.step({
-        name: `PUT /${index}/_create/1`,
+        name: `PUT /${ELASTIC_TEST_INDEX}/_create/1`,
         fn: async () => {
             const _id = '1'
 
             const document = await client.documents.index({
-                target: index,
+                target: ELASTIC_TEST_INDEX,
                 _id,
                 body: { title: 'Deno is great', popularity: 9.9 }
             })
 
             assertEquals(document.result, 'created')
-            assertEquals(document._index, index)
+            assertEquals(document._index, ELASTIC_TEST_INDEX)
             assertEquals(document._version, 1)
             assertEquals(document._id, _id)
         }
     })
 
     await t.step({
-        name: `GET /${index}/_doc/1`,
+        name: `GET /${ELASTIC_TEST_INDEX}/_doc/1`,
         fn: async () => {
             const _id = '1'
 
             const document = await client.documents.get({
-                index,
+                index: ELASTIC_TEST_INDEX,
                 _id
             })
 
-            assertEquals(document.found, true)
-            assertEquals(document._index, index)
+            assert(document.found)
+            assertEquals(document._index, ELASTIC_TEST_INDEX)
             assertEquals(document._id, _id)
         }
     })
 
     await t.step({
-        name: `GET /${index}/_doc/9999`,
+        name: `GET /${ELASTIC_TEST_INDEX}/_doc/9999`,
         fn: async () => {
             const _id = '9999'
 
             try {
                 await client.documents.get({
-                    index,
+                    index: ELASTIC_TEST_INDEX,
                     _id
                 })
             } catch (err) {
-                assertEquals(err.found, false)
-                assertEquals(err._index, index)
+                assert(!err.found)
+                assertEquals(err._index, ELASTIC_TEST_INDEX)
                 assertEquals(err._id, _id)
             }
         }
     })
 
-    await t.step({
-        name: 'delete indice that is created for document',
-        fn: async () => {
-            const indice = await client.indices.delete({ index })
-
-            assertEquals(indice.acknowledged, true)
-        }
-    })
+    // ![test_dependency]
+    await client.indices.delete({ index: ELASTIC_TEST_INDEX })
 })
--- tests/indices.test.ts
+++ tests/indices.test.ts
@@ -1,23 +1,21 @@
 import { client, ELASTIC_TEST_INDEX, assertEquals, assert } from './deps.ts'
 
 Deno.test('index apis', async (t) => {
-    const index = ELASTIC_TEST_INDEX
-
     await t.step({
-        name: `PUT /${index}`,
+        name: `PUT /${ELASTIC_TEST_INDEX}`,
         fn: async () => {
-            const indice = await client.indices.create({ index })
+            const indice = await client.indices.create({ index: ELASTIC_TEST_INDEX })
 
-            assertEquals(indice.acknowledged, true)
-            assertEquals(indice.index, index)
+            assert(indice.acknowledged)
+            assertEquals(indice.index, ELASTIC_TEST_INDEX)
         }
     })
 
     await t.step({
-        name: `PUT /${index}`,
+        name: `PUT /${ELASTIC_TEST_INDEX}`,
         fn: async () => {
             try {
-                await client.indices.create({ index })
+                await client.indices.create({ index: ELASTIC_TEST_INDEX })
 
                 assert(false, 'can\'t re-create an existing index')
             } catch (err) {
@@ -27,73 +25,73 @@ Deno.test('index apis', async (t) => {
     })
 
     await t.step({
-        name: `GET /${index}`,
+        name: `GET /${ELASTIC_TEST_INDEX}`,
         fn: async () => {
-            const indice = await client.indices.get({ target: index })
+            const indice = await client.indices.get({ target: ELASTIC_TEST_INDEX })
 
-            assertEquals(Object.hasOwn(indice, index), true)
+            assert(Object.hasOwn(indice, ELASTIC_TEST_INDEX))
         }
     })
 
     await t.step({
-        name: `HEAD /${index}`,
+        name: `HEAD /${ELASTIC_TEST_INDEX}`,
         fn: async () => {
-            const exists = await client.indices.exists({ target: index })
+            const exists = await client.indices.exists({ target: ELASTIC_TEST_INDEX })
 
-            assertEquals(exists, true)
+            assert(exists)
         }
     })
 
     await t.step({
-        name: `POST /${index}/_close`,
+        name: `POST /${ELASTIC_TEST_INDEX}/_close`,
         fn: async () => {
-            const indice = await client.indices.close({ index })
+            const indice = await client.indices.close({ index: ELASTIC_TEST_INDEX })
 
-            assertEquals(indice.acknowledged, true)
+            assert(indice.acknowledged)
         }
     })
 
     await t.step({
-        name: `POST /${index}/_open`,
+        name: `POST /${ELASTIC_TEST_INDEX}/_open`,
         fn: async () => {
-            const indice = await client.indices.open({ target: index })
+            const indice = await client.indices.open({ target: ELASTIC_TEST_INDEX })
 
-            assertEquals(indice.acknowledged, true)
+            assert(indice.acknowledged)
         }
     })
 
     await t.step({
-        name: `GET /${index}/_settings`,
+        name: `GET /${ELASTIC_TEST_INDEX}/_settings`,
         fn: async () => {
-            const indice = await client.indices.settings({ target: index })
+            const indice = await client.indices.settings({ target: ELASTIC_TEST_INDEX })
 
-            assertEquals(Object.hasOwn(indice, index), true)
+            assert(Object.hasOwn(indice, ELASTIC_TEST_INDEX))
         }
     })
 
     await t.step({
-        name: `DELETE /${index}`,
+        name: `DELETE /${ELASTIC_TEST_INDEX}`,
         fn: async () => {
-            const indice = await client.indices.delete({ index })
+            const indice = await client.indices.delete({ index: ELASTIC_TEST_INDEX })
 
-            assertEquals(indice.acknowledged, true)
+            assert(indice.acknowledged)
         }
     })
 
     await t.step({
-        name: `HEAD /${index}`,
+        name: `HEAD /${ELASTIC_TEST_INDEX}`,
         fn: async () => {
-            const exists = await client.indices.exists({ target: index })
+            const exists = await client.indices.exists({ target: ELASTIC_TEST_INDEX })
 
-            assertEquals(exists, false)
+            assert(!exists, 'deleted indice can\'t be exists')
         }
     })
 
     await t.step({
-        name: `DELETE /${index}`,
+        name: `DELETE /${ELASTIC_TEST_INDEX}`,
         fn: async () => {
             try {
-                await client.indices.delete({ index })
+                await client.indices.delete({ index: ELASTIC_TEST_INDEX })
 
                 assert(false, 'can\'t delete an index that is already deleted')
             } catch (err) {
