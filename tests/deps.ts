a4b466962ec217531c7c820459b5f736ddbf4534
dogukanakkaya <doguakkaya27@hotmail.com>
2022-03-29 22:23:29 +0300

    feat: update std library to 0.132.0

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,12 +1,12 @@
-export { assertEquals, assert } from 'https://deno.land/std@0.130.0/testing/asserts.ts'
-export { delay } from 'https://deno.land/std@0.130.0/async/delay.ts'
+export { assertEquals, assert } from 'https://deno.land/std@0.132.0/testing/asserts.ts'
+export { delay } from 'https://deno.land/std@0.132.0/async/delay.ts'
 import { Client as ElasticsearchClient } from '../mod.ts'
 
 export const ELASTIC_TEST_INDEX = 'my-test-index'
 export const NONEXISTENT_ELASTIC_TEST_INDEX = 'nonexistent-index'
 
 const client = new ElasticsearchClient({
-    node: 'https://localhost:9201',
+    node: 'https://localhost:9200',
     auth: {
         username: 'elastic',
         password: 'password'
